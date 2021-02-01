9e0efeedf773389a652f29f108a1eda06e6d4eab
fernandocfbf <fernandocfbf@al.insper.edu.br>
2021-02-01 16:47:32 -0300

    Configurando para deploy

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -9,7 +8,11 @@ require('dotenv').config()
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const port = process.env.PORT || 3003
+
+
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,28 +20,28 @@ app.set('view engine', 'ejs');
 
 app.use(cors())
 app.use(logger('dev'));
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({ limit: "50mb",extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: "50mb", extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get('/', (req, res) => {
+  res.send("API funcionando")
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.listen(port, () => {
+  console.log("Backend is running on port ", port)
+
+  if (process.env.NODE_ENV === undefined) {
+    console.log("Environment: development")
+  } else {
+    console.log("Environment: production")
+  }
+
+})
 
 module.exports = app;
