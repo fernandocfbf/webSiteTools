dc021c3422383e60fa3bff82783d3ef3eb28fe9f
unknown <fernando.fincatti@pay4fun01.local>
2021-07-21 20:28:40 -0300

    Corrigindo bug de periodo

--- functions/handleCutData.js
+++ functions/handleCutData.js
@@ -2,7 +2,7 @@ function handleCutData(period, arrayToCut) {
   //return the last {dataNumber} elements
   var cutPeriod = period*200
   if(period === '3'){
-    cutPeriod = arrayToCut.length
+    arrayToCut
   }
 
   return arrayToCut.slice(Math.max(arrayToCut.length - cutPeriod, 0))
