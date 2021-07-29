f80b1d5e7eac06aac8acbdbfb878a88b0bfee0c4
unknown <fernando.fincatti@pay4fun01.local>
2021-07-28 21:29:55 -0300

    get databse info

--- functions/sumAmounts.js
+++ functions/sumAmounts.js
@@ -3,7 +3,7 @@ const countries_coordinates = require('../utils/countries')
 function sumAmounts(data) {
     var years = {}
     var fields = {}
-    var countries = {}
+    var countries = []
     var investedSum = 0
     var peopleSum = 0
     for (var i = 0; i < data.length; i++) {
@@ -22,17 +22,17 @@ function sumAmounts(data) {
             }
         }
         if (el['gsx$country']['$t'].toString().length > 0 ){
-            if (countries.hasOwnProperty(el['gsx$country']['$t'])) {
-                countries[el['gsx$country']['$t']]['sum'] += 1
-            } else {
-                
+            var current = countries.find(elem => elem.country == el['gsx$country']['$t'])
+            if (current) {
+                current['sum'] += 1
+            } else {                
                 const country = countries_coordinates.find(elem => elem.country == el['gsx$country']['$t'])
-                console.log(el['gsx$country']['$t'], country)
-                countries[el['gsx$country']['$t']] ={
+                countries.push({
+                    'country': el['gsx$country']['$t'],
                     'sum': 1,
                     'latitude': country.latitude,
                     'longitude': country.longitude
-                }
+                })
             }
         }
 
